ac81af29d09634e48ab4327f17c306a37c9b6e96
Maksym-Saukh <129177688+Maksym-Saukh@users.noreply.github.com>
2023-07-05 11:59:59 +0300

    Update Task3.c

--- Task3.c
+++ Task3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    int p;
+    printf("Введіть кількість розрядів (p ≤ 30): ");// Вводимо кількість розрядів
+    scanf("%d", &p); // Зчитуємо в змінну
+
+    long long count = pow(2, p) - (p - 2); // Рахуємо за формулою
+
+    printf("Кількість чисел з %d розрядів: %lld\n", p, count); // Виводимо знаяення
+
+    return 0;
+}
